b6d3eec8753735da7596ade34f734c08cdc628dd
Rajdeep Sharma <rajdeep.sharma17@gmail.com>
2018-05-31 22:20:11 +0530

    updated UI

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#4fb4c6"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#4fb4c6",
         "opacity": 0.4,
         "width": 1
       },
